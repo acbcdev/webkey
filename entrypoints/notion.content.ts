305ccc36738ef6919e3031a69f16db1d62de63e3
acbc <acbc.dev@gmail.com>
2025-11-14 23:30:18 -0500

    refactor(notion): use centralized constants for selectors and shortcuts

--- entrypoints/notion.content.ts
+++ entrypoints/notion.content.ts
@@ -1,15 +1,13 @@
 import hotkeys from "hotkeys-js";
+import { SELECTORS, SHORTCUTS } from "@/lib/constants";
 import { $$ } from "@/lib/query";
-
-const ADD_NEW_ITEM_SELECTOR =
-  ".notion-gallery-view .notion-selectable.notion-collection_view-block div div";
 
 export default defineContentScript({
   matches: ["*://*.notion.so/*"],
   main() {
     // Press 'n' to click the add new item button
-    hotkeys("n", () => {
-      const list = $$<HTMLElement>(ADD_NEW_ITEM_SELECTOR);
+    hotkeys(SHORTCUTS.NOTION.ADD_NEW_ITEM, () => {
+      const list = $$<HTMLElement>(SELECTORS.NOTION.ADD_NEW_ITEM);
       if (list.length > 0) {
         const lastButton = list[list.length - 1];
         if (lastButton) {
